21e7226a78a87e912ce162e5992b98df8793fdb6
saravana.anbalagan <saravanan.anbalagan@cube.global>
2026-04-25 21:32:02 +0530

    First Home Assignment

---
+++ src/main/java/org/testleaf/Main.java
@@ -0,0 +1,9 @@
+package org.testleaf;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Welcome to TestLeaf");
+        System.out.println("dnlfksdnkfds") ;
+    }
+}
---
+++ src/main/java/org/testleaf/sample.java
@@ -0,0 +1,5 @@
+package org.testleaf;
+
+public class sample {
+    
+}
