8a514163736f1da156a1c45f23d4ad5bc3914c57
malinikarunanidhi22-hash <malinikarunanidhi22@gmail.com>
2026-05-02 23:23:51 +0530

    Delete src/main/java/org/testleaf/Main.java

--- src/main/java/org/testleaf/Main.java
+++
@@ -1,9 +0,0 @@
-package org.testleaf;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Welcome to TestLeaf");
-        System.out.println("dnlfksdnkfds") ;
-    }
-}
